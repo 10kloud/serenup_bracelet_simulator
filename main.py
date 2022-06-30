0b328d1e74d024c2efa6c074216132818ef1b3cd
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2022-06-30 09:23:55 +0200

    feat(settings): configuration file

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import boto3
-import uuid
-import os
 import json
 import math
-from secrets import randbelow
-from multiprocessing import Pool
+import os
 import time
+from multiprocessing import Pool
+from secrets import randbelow
 
-bracelet_count = 5
+import boto3
+import yaml
+
 bracelet_send_delay_in_seconds = 1
 bracelet_message_limit = 1_000_000
 
@@ -16,9 +16,9 @@ stream_name = os.environ.get("AWS_KINESIS_STREAM_NAME", None)
 kinesis = boto3.client('kinesis', region_name="eu-west-1")
 
 
-def simulate_smart_bracelet(_):
-    customer_id = str(uuid.uuid4())
-    device_id = str(uuid.uuid4())
+def simulate_smart_bracelet(bracelet: dict):
+    customer_id = bracelet["user_id"]
+    device_id = bracelet["bracelet_id"]
 
     # Random number between 50 and 120
     a = randbelow(70) + 50
@@ -30,8 +30,8 @@ def simulate_smart_bracelet(_):
             device_id=device_id,
             customer_id=customer_id,
             measured_at=time.time_ns(),
-            serendipity=(10.0 + 5.0 * math.sin(i)),
-            battery=float(a * math.sin(i // b) + b)
+            serendipity=round(float(10.0 + 5.0 * math.sin(i)), 4),
+            battery_level=round(float(a * math.sin(i // b) + b), 4)
         )
         kinesis.put_record(
             StreamName=stream_name,
@@ -44,6 +44,13 @@ def simulate_smart_bracelet(_):
     return device_id
 
 
+def read_config(filepath: str):
+    with open(filepath, 'r') as file:
+        config = yaml.safe_load(file)
+
+    return config
+
+
 def bootchecks():
     if not os.getenv("AWS_KINESIS_STREAM_NAME", False):
         print("Set AWS_KINESIS_STREAM_NAME env var first!")
@@ -51,11 +58,10 @@ def bootchecks():
 
 
 def main():
-    with Pool(bracelet_count) as pool:
-        pool.map(
-            simulate_smart_bracelet,
-            list(range(bracelet_count))
-        )
+    config = read_config("config.yml")
+    bracelets = config.get("bracelets")
+    with Pool(len(bracelets)) as pool:
+        pool.map(simulate_smart_bracelet, bracelets)
 
 
 if __name__ == "__main__":
